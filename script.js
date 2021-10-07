89cf889d4e912eeabc2a5bc2a63f63d6fb691dad
David Hovhannisyan <hovhannisyandav@icloud.com>
2021-10-07 13:11:00 +0400

    upload files

---
+++ script.js
@@ -0,0 +1,23 @@
+
+let color = prompt('Ընտրեք լուսացույցի գույնը (Հայ,Eng,Рус)');
+if (color == 'կանաչ') {
+    alert('Անցեք')
+}else if (color=='դեղին'){
+    alert('Սպասեք')
+}else if (color=='կարմիր') {
+    alert('Կանգնեք')
+}else if (color=='green') {
+    alert('go')
+}else if (color=='yellow') {
+    alert('wait')
+}else if (color=='red') {
+    alert('stop')
+}else if (color=='зеленый') {
+    alert('проходите')
+}else if (color=='желтый') {
+    alert('ждите')
+}else if (color=='красный') {
+    alert('стойте')
+}else {
+    alert('Սխալ մուտքագրում')
+}
